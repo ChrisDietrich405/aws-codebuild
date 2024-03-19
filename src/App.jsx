a398808aaed6c8ea759a8ba946cc971bfe743810
Chris <chrisdietrich3666@gmail.com>
2024-03-19 16:08:51 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        
-      </div>
-      <h1>Vite + React + Jhonson</h1>
+      <h1>Practicing with CodeBuild</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> hello
-        </p>
+        <p>Learning about CI/CD</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
